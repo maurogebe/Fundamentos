4131ea8a5bdbf580a7ab4c899d047cdfb6e4de1e
Mauricio Guerra <maurogebe.96@gmail.com>
2020-09-30 22:06:10 -0500

    Ejercicios de los nuevos metodos para reemplazar el for

--- JavaScript/ejercicio-4-incluyendo-html-con-js.js
+++ JavaScript/ejercicio-4-incluyendo-html-con-js.js
@@ -5,51 +5,94 @@ const cardInfo = document.getElementById('card-info')
 
 // Mostrar todos
 
-function getTableName() {
-    tableUsers.innerHTML = ''
-    for(var i = 0; i < users.length; i++) {
-        const row = `<tr>
-            <td>${users[i].name}</td>
-            <td>${users[i].age}</td>
-            <td>${users[i].email}</td>
-        </tr>`
-        tableUsers.innerHTML += row
-    }
-}
+// function getTableName() {
+//     tableUsers.innerHTML = ''
+//     for(var i = 0; i < users.length; i++) {
+//         const row = `<tr>
+//             <td>${users[i].name}</td>
+//             <td>${users[i].age}</td>
+//             <td>${users[i].email}</td>
+//         </tr>`
+//         tableUsers.innerHTML += row
+//     }
+// }
 
+// Nuevo metodo de Mostrar todos
+
+function getTableName() {
+        tableUsers.innerHTML = ''
+        users.forEach((user) => {
+            const row = `<tr>
+                            <td>${user.name}</td>
+                            <td>${user.age}</td>
+                            <td>${user.email}</td>
+                        </tr>`
+            tableUsers.innerHTML += row
+        })
+}
 
 // Mostrar Mayores de 20 y menores de 40
 
+// function getTableAge() {
+//     tableUsers.innerHTML = ''
+//     for(var i = 0; i < users.length; i++) {
+//         if((users[i].age >= 20) && (users[i].age <= 40)) {
+//             const row = `<tr>
+//                 <td>${users[i].name}</td>
+//                 <td>${users[i].age}</td>
+//                 <td>${users[i].email}</td>
+//             </tr>`
+//             tableUsers.innerHTML += row
+//         }
+//     }
+// }
+
+// Nuevo metodo de mostrar Mayores de 20 y menores de 40
+
 function getTableAge() {
     tableUsers.innerHTML = ''
-    for(var i = 0; i < users.length; i++) {
-        if((users[i].age >= 20) && (users[i].age <= 40)) {
-            const row = `<tr>
-                <td>${users[i].name}</td>
-                <td>${users[i].age}</td>
-                <td>${users[i].email}</td>
-            </tr>`
+    const age = users.filter((user) => user.age >= 20 && user.age <= 40)
+    age.forEach((user) => {
+        const row = `<tr>
+                        <td>${user.name}</td>
+                        <td>${user.age}</td>
+                        <td>${user.email}</td>
+                    </tr>`
             tableUsers.innerHTML += row
-        }
-    }
+    })
 }
 
 
 // Mostrando emails de Academlo
 
+// function getTableEmail() {
+//     tableUsers.innerHTML = ''
+//     for(var i = 0; i < users.length; i++) {
+//         if(users[i].email.endsWith('academlo.com')) {
+//             const row = `<tr>
+//                 <td>${users[i].name}</td>
+//                 <td>${users[i].age}</td>
+//                 <td>${users[i].email}</td>
+//             </tr>`
+//             tableUsers.innerHTML += row
+//         }
+//     }
+    
+// }
+
+// Nuevo Metodo de Mostrando emails de Academlo
+
 function getTableEmail() {
     tableUsers.innerHTML = ''
-    for(var i = 0; i < users.length; i++) {
-        if(users[i].email.endsWith('academlo.com')) {
-            const row = `<tr>
-                <td>${users[i].name}</td>
-                <td>${users[i].age}</td>
-                <td>${users[i].email}</td>
-            </tr>`
+    const emailAcademlo = users.filter((user) => user.email.endsWith('academlo.com'))
+    emailAcademlo.forEach((user) => {
+        const row = `<tr>
+                        <td>${user.name}</td>
+                        <td>${user.age}</td>
+                        <td>${user.email}</td>
+                    </tr>`
             tableUsers.innerHTML += row
-        }
-    }
-    
+    })
 }
 
 // Mostrar Redes Sociales
@@ -57,13 +100,13 @@ function getTableEmail() {
 function getCardSocial() {
     tableUsers.innerHTML = ''
     let cardDescription = ''
-    for(var i = 0; i < users.length; i++) {
+    for(let i = 0; i < users.length; i++) {
         let social = users[i].social
         cardDescription += `<div class="card" style="width: 18rem;">
             <div class="card-body">
             <h5 class="card-title">${users[i].name}</h5>
             <p class="card-text">${users[i].age}</p>`
-        for(var j = 0; j < social.length; j++) {
+        for(let j = 0; j < social.length; j++) {
             cardDescription += `<a href="#" class="btn btn-primary">${social[j].url}</a>`
         }
         cardDescription += `<p class="card-text">${users[i].gender}</p>
@@ -74,19 +117,60 @@ function getCardSocial() {
 
 // Mostrar Mujeres
 
+// function getTableFemale() {
+//     tableUsers.innerHTML = ''
+//     for(var i = 0; i < users.length; i++) {
+//         if(users[i].gender === 'Female') {
+//             const row = `<tr>
+//                 <td>${users[i].name}</td>
+//                 <td>${users[i].age}</td>
+//                 <td>${users[i].email}</td>
+//             </tr>`
+//             tableUsers.innerHTML += row
+//         }
+//     }
+// }
+
+// Nuevo metodo de Mostrar Mujeres
+
 function getTableFemale() {
     tableUsers.innerHTML = ''
-    for(var i = 0; i < users.length; i++) {
-        if(users[i].gender === 'Female') {
-            const row = `<tr>
-                <td>${users[i].name}</td>
-                <td>${users[i].age}</td>
-                <td>${users[i].email}</td>
-            </tr>`
+    const woman = users.filter((user) => user.gender === 'Female')
+    woman.forEach((user) => {
+        const row = `<tr>
+                        <td>${user.name}</td>
+                        <td>${user.age}</td>
+                        <td>${user.email}</td>
+                    </tr>`
             tableUsers.innerHTML += row
-        }
-    }
+    })
 }
+
+// Utilice map para que se viera que funcionaba
+// function getTableFemale() {
+//     tableUsers.innerHTML = ''
+//     const woman = users.map((user) => {
+//         if(user.gender === 'Female') {
+//             return `<tr>
+//                         <td>${user.name}</td>
+//                         <td>${user.age}</td>
+//                         <td>${user.email}</td>
+//                     </tr>`
+//         }
+//     })
+//     tableUsers.innerHTML += woman
+// }
+
+// Map
+
+// crea un array nuevo con el return de la función anónima por cada elemento
+// const emails = users.map((user) => {
+//     return user.age * 2
+// });
+// console.log(emails);
+
+// const emails = users.map((user) => user.age * 2);
+// const ages = [1, 2, 3, 4 ,5];
 
 
 window.getTableName = getTableName
--- JavaScript/ejercicios-2.js
+++ JavaScript/ejercicios-2.js
@@ -28,64 +28,30 @@ function birthdayCakeCandles(candles) {
     var total = 0;
     var temporary = 0
     var temporary2 = 0
-    var temporary3 = 0
     for(var i = 0; i < candles.length; i++) {
-        if((candles[0] != candles[candles.length]) && (candles[0] != candles[candles.length / 2])) {
-            if(i === 0) {
-                temporary = candles[i]
-                for(var k = 0; k < candles.length; k++) {
-                    if(temporary >= candles[k]) {
-                        temporary3 = temporary
-                    } else if(temporary < candles[k]) {
-                        temporary3 = candles[k]
-                        break
-                    }
-                }
-                if(temporary >= temporary3) {
-                    total += 1
-                    temporary3 = temporary
-                    continue
-                } else if(temporary < temporary3) {
-                    temporary3 = temporary
-                    continue
-                }
-            } else if(candles[i] >= candles[i - 1]) {
-                temporary = candles[i]                
-            } else if(candles[i] < candles[i - 1]) {
-                temporary = candles[i - 1]
-                continue
-            }
-
-            if(temporary < temporary3) {
-                continue
-            } else if(temporary >= candles[i]) {
-                for(var j = 0; j < candles.length; j++) {
-                    if(candles[j] < temporary3) {
-                        continue
-                    } else if(temporary >= candles[j]) {
-                        temporary2 = temporary
-                    } else if(temporary < candles[j]) {
-                        temporary2 = 0
-                        break
-                    }
+        if(i === 0) {
+            temporary = candles[i]
+            for(var k = 0; k < candles.length; k++) {
+                if(temporary >= candles[k]) {
+                    temporary2 = temporary
+                } else if(temporary < candles[k]) {
+                    temporary2 = candles[k]
+                    temporary = candles[k]
                 }
             }
-
-            if(temporary === temporary2) {
-                temporary3 = temporary
+            if(candles[i] >= temporary2) {
                 total += 1
+                continue
+            } else if(candles[i] < temporary2) {
+                continue
             }
-        } else {
-            for(var i = 0; i < candles.length; i++) {
-                if(i === 0) {
-                    temporary = candles[i]
-                } else if(candles[i] === candles[i - 1]) {
-                    temporary = candles[i]
-                }
-            }
+        } else if(candles[i] >= temporary2) {
+            total += 1
+            continue
+        } else if(candles[i] < temporary2) {
+            continue
         }
     }
-
     return total
 
 }
